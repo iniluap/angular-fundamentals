20661e0cd33c0efcba1448785c4a19c7a8f6782a
psjakubowska <paulina.sedlak.jakubowska@instapage.com>
2020-07-22 22:18:42 +0200

    Complete one way data flow with event emmiters

--- app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
+++ app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnChanges, Input, Output, EventEmitter } from '@angular/core';
 
 import { Passenger } from '../../models/passenger.interface';
 
@@ -15,8 +15,14 @@ import { Passenger } from '../../models/passenger.interface';
                 class="status"
                 [style.backgroundColor]="(detail.checkedIn ? 'green' : 'red')">
             </span>
-                {{ detail.fullname }}
-            <p>{{ detail | json }}</p>
+                <div *ngIf="editing">
+                    <input
+                        type="text"
+                        [value]="detail.fullname"
+                        (input)="onNameChange(name.value)"
+                        #name>
+                </div>
+                <div *ngIf="!editing">{{ detail.fullname }}</div>
             <div class="date">
                 Check in date:
                 {{ detail.checkedIn ? (detail.checkInDate | date: 'yMMMd' | uppercase ) : 'Not checked in' }}
@@ -24,11 +30,46 @@ import { Passenger } from '../../models/passenger.interface';
             <div class="children">
                 Children: {{ detail.children?.length || 0 }}
             </div>
+            <button (click)="toggleEdit()">
+                {{ editing ? 'Done' : 'Edit'}}
+            </button>
+            <button (click)="onRemove()">
+                Remove
+            </button>
         </div>
     `
 })
-export class PassengerDetailComponent {
+export class PassengerDetailComponent implements OnChanges {
     @Input()
     detail: Passenger;
+
+    editing: boolean = false;
+
+    @Output()
+    edit: EventEmitter<any> = new EventEmitter();
+
+    @Output()
+    remove: EventEmitter<any> = new EventEmitter();
+
     constructor() {}
+
+    ngOnChanges(changes) {
+        console.log(changes);
+        if (changes.detail) {
+            this.detail = Object.assign({}, changes.detail.currentValue);
+        }
+    }
+
+    onNameChange(value: string) {
+        this.detail.fullname = value;
+    }
+    toggleEdit() {
+        if(this.editing) {
+            this.edit.emit(this.detail);
+        }
+        this.editing = !this.editing;
+    }
+    onRemove() {
+        this.remove.emit(this.detail);
+    }
 }
--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -12,7 +12,9 @@ import { Passenger } from '../../models/passenger.interface';
             </passenger-count>
             <passenger-detail
                 *ngFor="let passenger of passengers;"
-                [detail]="passenger">
+                [detail]="passenger"
+                (edit)="handleEdit($event)"
+                (remove)="handleRemove($event)">
             </passenger-detail>
         </div>
     `
@@ -52,5 +54,23 @@ export class PassengerDashboardComponent implements OnInit {
             checkInDate: null,
             children: null
         }];
+    }
+
+    handleEdit(event: Passenger) {
+        this.passengers = this.passengers.map(
+            (passenger: Passenger) => {
+                if(passenger.id === event.id) {
+                    passenger = Object.assign({}, passenger, event);
+                }
+                return passenger;
+            }
+        )
+    }
+    handleRemove(event: Passenger) {
+        this.passengers = this.passengers.filter(
+            (passenger: Passenger) => {
+                return passenger.id !== event.id;
+            }
+        )
     }
 }
